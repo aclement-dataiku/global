12c0c05d1227b8fb360d76d9784c41502dd4503c
Anita Clement <anitaclement@macbook-pro-3.home>
2021-02-14 20:36:17 +0000

    utils.py

---
+++ utils.py
@@ -0,0 +1,20 @@
+def seed_reproducer(seed=2020):
+    """Reproducer for pytorch experiment.
+    Parameters
+    ----------
+    seed: int, optional (default = 2019)
+        Radnom seed.
+    Example
+    -------
+    seed_reproducer(seed=2019).
+    """
+    random.seed(seed)
+    os.environ["PYTHONHASHSEED"] = str(seed)
+    np.random.seed(seed)
+    torch.manual_seed(seed)
+    if torch.cuda.is_available():
+        torch.cuda.manual_seed(seed)
+        torch.cuda.manual_seed_all(seed)
+        torch.backends.cudnn.deterministic = True
+        torch.backends.cudnn.benchmark = False
+        torch.backends.cudnn.enabled = True
